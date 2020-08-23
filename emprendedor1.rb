deb270e8b727da8a863f40af2c447ad042613233
maria alvarez <mar.alvareg@gmail.com>
2020-08-23 15:07:34 -0400

    usuarios normales

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -3,8 +3,9 @@ users = ARGV[1].to_i
 cost = ARGV[2].to_i
 
 util = value*users-cost
-    if util.positive?
-        util = util*0.65
-    end
+
+if util.positive?
+    util = util*0.65
+end
 
 puts util
--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,14 +1,14 @@
 value = ARGV[0].to_i
-users = ARGV[1].to_i
-user_p = ARGV[2].to_i
-user_g =ARGV[3].to_i
+users_n = ARGV[1].to_i #normales
+user_p = ARGV[2].to_i #premium
+user_g =ARGV[3].to_i #gratis
 cost = ARGV[4].to_i
 
-c_users= user_p*value*2
+c_users= (user_p*value*2)+(user_n*value)
+util=c_users-cost
 
-    util=c_users-cost
-    if util.positive?
-        util = util*0.65
-    end
+if util.positive?
+    util = util*0.65
+end
 
 puts util
